ff87d3295cf9eb56da1d6408120650113a1a4af8
Alexandre WEISSER <alexandre.weisser@gmail.com>
2022-04-15 15:54:09 +0200

    feat : final mep

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Quick Online Pastebin cause moving things from one pc to another is a bitch
 
-Now dockerized and prettyfied !
+Now dockerized, prettified, and with SSL !
+
+Go watch it in prod : [https://pad.hosh.it](https://pad.hosh.it)
+
+
+# Development
 
 build with ``build.cmd``
+deploy however you want but and example is in `deploy.cmd`
--- mount-image.sh
+++ mount-image.sh
@@ -5,5 +5,7 @@
 rm instantshit.tar
 gzip -d instantshit.tar.gz
 docker stop InstantShit && docker rm InstantShit || docker rm InstantShit
+docker stop InstantShitMom && docker rm InstantShitMom || docker rm InstantShitMom
 docker load < instantshit.tar
 docker run -d -it -p 12121:80 --name InstantShit instantshit
+docker run -d -it -p 12122:80 --name InstantShitMom instantshit
